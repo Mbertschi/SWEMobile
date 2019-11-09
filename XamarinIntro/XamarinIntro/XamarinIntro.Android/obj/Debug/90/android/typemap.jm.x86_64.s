5c7331bc110209a90d510a247d5e7823a7ae0e84
Mbertschi <michibertschi@gmail.com>
2019-11-09 11:53:43 +0100

    anfang taschenrechner
    anfang Taschenrechner

---
+++ XamarinIntro/XamarinIntro/XamarinIntro.Android/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: ed95f6fe41c344a3383cdc7a5d8622c9057160ff */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
---
+++ XamarinIntro/XamarinIntro/XamarinIntro/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinIntro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private int number1 ;
+        private int number2 ;
+        private int sumary ;
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Gleich_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Plus_Clicked(object sender, EventArgs e)
+        {
+            sumary = number1 + number2; 
+            
+        }
+
+        private void Button_0_Clicked(object sender, EventArgs e)
+        {
+            if (number1 == null)
+            {
+                number1 = 0;
+            }
+            else { number2 = 0; }
+        }
+
+        private void Button_1_Clicked(object sender, EventArgs e)
+        {
+            
+            Sumary.Text = 1.ToString;
+        }
+    }
+}
