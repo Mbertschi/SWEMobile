f75d8cf2108dee82bbe1740483f97182ddca2e77
Mbertschi <michibertschi@gmail.com>
2019-11-10 17:41:49 +0100

    implements operationen

---
+++ XamarinIntro/XamarinIntro/XamarinIntro/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinIntro
+
+
+{
+    public enum Operator
+    {
+        Addition = 1,
+        Subtraction = 2,
+        Multipication = 3,
+        Division = 4
+    }
+
+    interface ICalculator
+    {
+         void AddNumber(int inNumber);
+
+         void AddOperator(Operator inOpterator);
+
+        String getSumary();
+        void resetValues();
+
+
+    }
+    class Calculator : ICalculator
+
+    {
+
+        private Operator calct;
+        private int addedNumber1 = 0;
+        private int addedNumber2 = 0;
+        private int sumary;
+
+
+
+        public void AddNumber(int inNumber) {
+
+
+            addedNumber1 = addedNumber1*10+inNumber; 
+
+        }
+
+        public void AddOperator(Operator inOperator)
+        {
+            calct = inOperator;
+            if(addedNumber2 == 0)
+            {
+                addedNumber2 = addedNumber1;
+                addedNumber1 = 0;
+            }
+            
+        }
+
+        public void resetValues()
+        {
+            addedNumber1 = 0;
+            addedNumber2 = 0;
+        }
+
+        private void calculate( int number1 , int numberTwo, Operator inoperator)
+        {
+            int i = (int)inoperator;
+             switch (i)
+            {
+                case 1:
+                   
+                    sumary = number1 + numberTwo;
+                    break;
+                case 2:
+                    sumary = number1 - numberTwo;
+                    break;
+                case 3:
+                    sumary = number1 * numberTwo;
+                    break;
+                case 4:
+                    if (numberTwo == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sumary = number1 / numberTwo;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public String getSumary(){
+          calculate(addedNumber2, addedNumber1, calct);
+            addedNumber2 = sumary;
+            addedNumber1 = 0;
+     
+            return sumary.ToString();
+        }
+
+
+
+        }
+    }
+
--- XamarinIntro/XamarinIntro/XamarinIntro/MainPage.xaml.cs
+++ XamarinIntro/XamarinIntro/XamarinIntro/MainPage.xaml.cs
@@ -13,34 +13,130 @@ namespace XamarinIntro
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        private int number1 ;
-        private int number2 ;
-        private int sumary ;
+        private ICalculator icalucate = new Calculator();
+
         public MainPage()
         {
             InitializeComponent();
-           
+         
         }
+
+        
+        
 
         private void Button_Gleich_Clicked(object sender, EventArgs e)
         {
             
+            Sumary.Text = icalucate.getSumary();
         }
 
         private void Button_Plus_Clicked(object sender, EventArgs e)
         {
-
-            Sumary.Text = Sumary.Text + "+";
+            icalucate.AddOperator(Operator.Addition);
+            Sumary.Text += "+";
+            
         }
 
         private void Button_0_Clicked(object sender, EventArgs e)
         {
-            Sumary.Text = Sumary.Text + 0.ToString();
+            icalucate.AddNumber(0);
+            Sumary.Text += 0.ToString();
         }
 
         private void Button_1_Clicked(object sender, EventArgs e)
         {
-            Sumary.Text = Sumary.Text + 1.ToString();
+            icalucate.AddNumber(1);
+            Sumary.Text += 1.ToString();
+        }
+
+        private void Label_4_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(4);
+            Sumary.Text += 4.ToString();
+        }
+
+        private void Label_3_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(3);
+            Sumary.Text += 3.ToString();
+        }
+
+        private void Label_5_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(5);
+            Sumary.Text += 5.ToString();
+        }
+
+        private void Label_6_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(6);
+            Sumary.Text += 6.ToString();
+        }
+
+        private void Label_7_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(7);
+            Sumary.Text += 7.ToString();
+        }
+
+        private void Label_8_Clicked(object sender, EventArgs e)
+        {
+
+            icalucate.AddNumber(8);
+           
+            Sumary.Text += 8.ToString();
+        }
+
+        private void Label_9_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(9);
+            Sumary.Text += 9.ToString();
+        }
+
+        private void Label_Coma_Clicked(object sender, EventArgs e)
+        {
+            
+            Sumary.Text += ",";
+        }
+
+        private void Label_Mal_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddOperator(Operator.Multipication);
+            Sumary.Text += "*";
+        }
+
+        private void Button_Durch_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddOperator(Operator.Division);
+            Sumary.Text += "/";
+        }
+
+        private void Button_AC_Clicked(object sender, EventArgs e)
+        {
+            Sumary.Text = "";
+            icalucate.resetValues();
+        }
+
+        private void Button_Prozent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_PlusMinus_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Label_2_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddNumber(2);
+            Sumary.Text += 2.ToString();
+        }
+
+        private void Button_Minus_Clicked(object sender, EventArgs e)
+        {
+            icalucate.AddOperator(Operator.Subtraction);
+            Sumary.Text += "-";
         }
     }
 }
